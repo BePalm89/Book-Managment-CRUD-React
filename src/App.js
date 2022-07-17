4d287eaaa5ba0404352c2c5f93c882867a53982c
ClaPalm <cpalmerini@ohpen.com>
2022-07-17 23:07:59 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import "./App.css";
 import "antd/dist/antd.css";
 import Navbar from "./components/navbar/Navbar";
 import ListBook from "./components/listBook/ListBook";
+import NewBook from "./components/newBook/NewBook";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<ListBook />} />
+          <Route path="/add" element={<NewBook />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,38 +1,25 @@
 import React from "react";
-import { Router, Switch, Route, Link } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import { Menu } from "antd";
 import { BookOutlined, FileAddOutlined } from "@ant-design/icons";
 import "./Navbar.css";
-import ListBook from "../listBook/ListBook";
-import NewBook from "../newBook/NewBook";
 
 const Navbar = () => {
   return (
-    <Router>
+    <div>
       <Menu
         mode="horizontal"
         className="centerItem"
         defaultSelectedKeys={["book"]}
       >
         <Menu.Item key="book" icon={<BookOutlined />}>
-          You library
-          <Link to="/" />
+          <Link to="/">You library</Link>
         </Menu.Item>
         <Menu.Item key="addBook" icon={<FileAddOutlined />}>
-          Add book
-          <Link to="/add" />
+          <Link to="/add">Add book</Link>
         </Menu.Item>
       </Menu>
-
-      <Switch>
-        <Route exact path="/">
-          <ListBook />
-        </Route>
-        <Route path="/add">
-          <NewBook />
-        </Route>
-      </Switch>
-    </Router>
+    </div>
   );
 };
 
